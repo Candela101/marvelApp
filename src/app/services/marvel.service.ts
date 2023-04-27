79683deb60472a13fb29017ca4c7e234b42f28eb
Candela101 <cabero01@floridauniversitaria.es>
2023-04-27 11:50:23 +0200

    creo el servicio para comunicarme con la api

---
+++ src/app/services/marvel.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+const URL_API = environment.urlApi;
+const KEY_PUBLIC = environment.keyPublic;
+const HASH = environment.hash;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarvelService {
+
+  constructor(private http: HttpClient) { }
+
+  getCharacters(): Observable<any> {
+    return this.http.get<any>(`${URL_API}/characters?ts=1&apikey=${KEY_PUBLIC}&hash=${HASH}`).pipe(map((data: any) => data.data.results));
+  }
+
+  getCharacter(id: string): Observable<any> {
+    return this.http.get(`${URL_API}/characters/${id}?ts=1&apikey=${KEY_PUBLIC}&hash=${HASH}`).pipe(map((data: any) => data.data.results));
+  }
+
+  getComics(): Observable<any> {
+
+    return this.http.get<any>(`${URL_API}/comics?ts=1&apikey=${KEY_PUBLIC}&hash=${HASH}`).pipe(map((data: any) => data.data.results));
+
+  }
+
+
+  getComic(id: string): Observable<any> {
+
+    return this.http.get(`${URL_API}/comics/${id}?ts=1&apikey=${KEY_PUBLIC}&hash=${HASH}`).pipe(map((data: any) => data.data.results));	
+
+  }
+}
