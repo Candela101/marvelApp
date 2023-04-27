02230125bc2e9514a7e02aa8d5c311cdb6ee116d
Candela101 <cabero01@floridauniversitaria.es>
2023-04-27 11:50:00 +0200

    creo las constante para mayor seguridad

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    urlApi: 'https://gateway.marvel.com:443/v1/public/',
+    keyPublic: '76db8ae43ccdbff97baceb7f9855d527',
+    hash: '91987bf5621048b1a6611512181006f7',
+    production: false
+};
