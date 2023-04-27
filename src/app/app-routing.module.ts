e69c1079a3380a299a3e252c38ef782272fbe7d8
Candela101 <cabero01@floridauniversitaria.es>
2023-04-27 11:45:17 +0200

    añado las rutas necesarias para la app y modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { ComicsComponent } from './pages/comics/comics.component';
+import { ComicComponent } from './pages/comic/comic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'characters', component:CharactersComponent},
+  {path:'character/:id', component:CharacterComponent},
+  {path:'comics', component:ComicsComponent},
+  {path:'comic/:id', component:ComicComponent},
+
+  {path:'', pathMatch:'full', redirectTo:'/characters'},
+  {path:'**', pathMatch:'full', redirectTo:'/characters'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { ComicsComponent } from './pages/comics/comics.component';
+import { ComicComponent } from './pages/comic/comic.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    CharacterComponent,
+    ComicsComponent,
+    ComicComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CommonModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
