9b352f26e94df6687a908589f133dd31691df9fd
Candela101 <cabero01@floridauniversitaria.es>
2023-05-16 21:42:56 +0200

    Añado el metodo de favoritos a comics

--- src/app/pages/comics/comics.component.ts
+++ src/app/pages/comics/comics.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { MarvelService } from 'src/app/services/marvel.service';
 
 @Component({
@@ -11,12 +12,12 @@ import { MarvelService } from 'src/app/services/marvel.service';
 export class ComicsComponent implements OnInit {
 
   title = 'Comics';
-
-  constructor(private marvelSvc: MarvelService, private router: Router) { }
   comics: any[] = [];
   limit: number = 20;
   offset: number = 0;
+  favorites: any[] = [];
 
+  constructor(private marvelSvc: MarvelService, private router: Router, private localStorage: LocalStorageService) { }
 
   ngOnInit() {
 
@@ -26,7 +27,12 @@ export class ComicsComponent implements OnInit {
   getAllComics() {
 
     this.marvelSvc.getComics(this.limit, this.offset).subscribe(data => {
-      this.comics = data;
+      const favorites = this.localStorage.getItem('favorites') || [];
+
+      this.comics = data.map((comic: any) => {
+        comic.isFavorite = favorites.some((fav: any) => fav.id === comic.id);
+        return comic;
+      });
     });
 
   }
@@ -49,5 +55,43 @@ export class ComicsComponent implements OnInit {
 
   getCurrentPage(): number {
     return Math.floor(this.offset / this.limit) + 1;
+  }
+
+  addToFavorites(comic: any) {
+    const favorites = this.localStorage.getItem('favorites') || [];
+    if (favorites.findIndex((fav: any) => fav.id === comic.id) === -1) {
+      favorites.push(comic);
+      this.localStorage.setItem('favorites', favorites);
+    }
+  }
+
+  removeFromFavorites(comic: any) {
+    const favorites = this.localStorage.getItem('favorites') || [];
+    const index = favorites.findIndex((fav: any) => fav.id === comic.id);
+    if (index !== -1) {
+      favorites.splice(index, 1);
+      this.localStorage.setItem('favorites', favorites);
+    }
+  }
+
+  isFavorite(comic: any): boolean {
+    return this.favorites.some((fav) => fav.id === comic.id);
+  }
+
+  saveFavorite() {
+    this.localStorage.setItem('favorites', this.favorites);
+  }
+
+  loadFavorites(comic: any) {
+    this.favorites = this.localStorage.getItem('favorites') || [];
+  }
+
+  toggleFavorite(comic: any) {
+    comic.isFavorite = !comic.isFavorite;
+    if (comic.isFavorite) {
+      this.addToFavorites(comic);
+    } else {
+      this.removeFromFavorites(comic);
+    }
   }
 }
