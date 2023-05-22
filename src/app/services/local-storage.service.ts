a2aa3c4dda95aa3177ec3e8f1c8353e489cbff1e
Candela101 <cabero01@floridauniversitaria.es>
2023-05-22 12:18:06 +0200

    añado efecto zoom a las cards

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -10,7 +10,7 @@ export class LocalStorageService {
   getItem(key: string): any {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : null;
-  }
+  } 
 
   setItem(key: string, value: any) {
     localStorage.setItem(key, JSON.stringify(value));
